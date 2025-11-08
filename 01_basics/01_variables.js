a241413ef24637a23249539ff0054e18839c42d5
ishuee <146664170+ishuee@users.noreply.github.com>
2025-11-08 13:12:31 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=14453
+let accountEmail="isha@google.com"
+var acccountPassword="12345"
+accountCity="Jaipur"
+let accountState
+
+//accountId=2
+
+accountEmail="issuee@gmail.com"
+acccountPassword="2121211"
+accountCity="kolkata"
+
+console.log(accountId);
+
+/*
+prefer not to use var because of issue in block scope and functional scope 
+*/
+console.table([accountEmail,accountId,accountCity,acccountPassword,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer JS 
+
+//alert("hello") //since we are using node JS and not browser alert cannot be used
+
+console.log(3
+    +3) //not preferred since code should be readable
+
+let name="ishaa"
+let age=22
+let isLoggedIn=true
+let state=null
+
+//PRIMITIVE DATA TYPES
+//number =>2^53
+//bigInt
+//string =>""
+//boolean =>true/false
+//null =>standalone value
+//undefined =>
+//symbol =>unique
+
+
+//object
+
+console.log(typeof null) //output will be null
+console.log(typeof undefined) //output will be undefined
