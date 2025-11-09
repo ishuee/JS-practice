a4cc70d88e03a5bcb84193160e58682309a9f646
ishuee <146664170+ishuee@users.noreply.github.com>
2025-11-09 20:14:11 +0000

    data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -10,17 +10,36 @@ let age=22
 let isLoggedIn=true
 let state=null
 
+const id=Symbol('123')
+const ide=Symbol('123')
+console.log(id==ide) //false 
+
+const n=34465634665673n
+console.log(typeof n) //bigInt
+
+console.log(typeof null) //output will be object
+console.log(typeof undefined) //output will be undefined
+
 //PRIMITIVE DATA TYPES
 //number =>2^53
 //bigInt
 //string =>""
 //boolean =>true/false
 //null =>standalone value
-//undefined =>when we do not specify aby value to the variable
+//undefined =>when we explicitly give value as undefined or do not specify any value to the variable
 //symbol =>unique
 
+const objs =["man","apples",1,false] //array can have value of different types 
+let myObj={
+    name:"isha",
+    age:23
+} //object
+console.log(typeof myObj)
+const myFunc=function(){
+    console.log("helloo");
+} //function
 
-//object
-
-console.log(typeof null) //output will be null
-console.log(typeof undefined) //output will be undefined
+//NON PRIMITIVE DATA TYPES (**data type will be functions for all**)
+//Arrays
+//objects
+//functions
