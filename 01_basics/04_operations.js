e614dac528908e7bedd3835dba82998880fc5ca1
ishuee <146664170+ishuee@users.noreply.github.com>
2025-11-09 19:38:49 +0000

    basic operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,14 @@
+console.log("1"+2)  //12
+console.log(2+"1")  //21
+
+console.log("1"+2+2)  //122
+console.log(2+2+"1") //41
+
+console.log(3+1*4%6) //7
+
+console.log(+true);  //1
+console.log(+false) //0
+
+let a=11;
+console.log(++a)  //11 
+//postfix prefix allowed
