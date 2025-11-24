d5abf170940e8cf94c48f1757b4f2e31c75d1d5d
ishuee <146664170+ishuee@users.noreply.github.com>
2025-11-24 15:53:31 +0000

    truthy and falsy

---
+++ 04_ControlFlow/truthy.js
@@ -0,0 +1,11 @@
+const email="isha@gmail.com"
+
+if(email) console.log("got user email")
+else console.log("did not get user email")
+
+//falsy values: always considered false
+//false,0,-0,BigInt 0n,"", null,undefined,NaN
+
+//truthy values : always considered true
+//"0", 'false'," ",[],{},function(){}
+
