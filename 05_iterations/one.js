845a96fd0e41e999096bce6c562531ab4e5ec912
ishuee <146664170+ishuee@users.noreply.github.com>
2025-11-24 16:09:05 +0000

    for while and do while loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,22 @@
+//for loop
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element)
+}
+
+//while loop
+
+let i=0
+while(i<=10) {
+    console.log(i)
+    i+=2
+}
+
+//do-while loop
+
+let score=1
+do{
+    console.log(score)
+    score++
+}while(score<=10)
