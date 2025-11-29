5e8177869ea1a581d48c09ff400a75eccf82cf8b
ishuee <146664170+ishuee@users.noreply.github.com>
2025-11-29 13:47:42 +0000

    more loop concepts

---
+++ 05_iterations/two.js
@@ -0,0 +1,56 @@
+//for of
+
+const arr=[1,2,3,4,5]
+
+for (const i of arr) {
+    console.log(i) //1 2 3 4 5 
+}
+
+const greetings="helloo"
+for (const greet of greetings) {
+     console.log(greet) //h e l l o o
+}
+
+//Maps
+
+const map=new Map()
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('JP',"Japan")
+map.set('IN',"India") //this entry will not be printed twice
+
+console.log(map) 
+/*
+Map(3) {
+  'IN' => 'India',
+  'USA' => 'United States of America',
+  'JP' => 'Japan'
+} */
+
+  for (const key of map) {
+    console.log(map)
+  }
+  /*
+Map(3) {
+  'IN' => 'India',
+  'USA' => 'United States of America',
+  'JP' => 'Japan'
+} */
+
+ for (const [key,value] of map) {
+    console.log(key,':', value)
+  } 
+  /*
+  IN : India
+USA : United States of America
+JP : Japan */
+
+const myObject ={
+    'game1':'NFS',
+    'gsme2':'SpiderMan'
+}
+
+//for (const [key,vlaue] of MyObject) {
+    console.log(key,':', value)
+}
+//this will not be executed as objects cannot be iterated like this
